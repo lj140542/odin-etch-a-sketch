b0b20b52721b019c272fcad9884341e54b031487
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-16 23:56:01 +0200

    Implement the drawing of the 16 by 16 default div grid.

---
+++ script.js
@@ -0,0 +1,19 @@
+const nbrOfCol = 16;
+const nbrOfRow = 16;
+const container = document.getElementById('container');
+
+for(let iRow = 1; iRow <= nbrOfRow; iRow++) {
+    let row = document.createElement('div')
+    row.classList.add('row');
+
+    for(let iCol = 1; iCol <= nbrOfCol; iCol++) {
+        let col = document.createElement('div');
+        col.classList.add('col');
+
+        row.appendChild(col);
+    }
+
+    container.appendChild(row);
+}
+
+
