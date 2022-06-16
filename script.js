ec99e6ebc44f73515e4498716c77a6e07e48ed63
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-17 00:09:07 +0200

    Add the "drawing" feature.
    The "drawing" feature consists of the background coloration any the hovered div.

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ for(let iRow = 1; iRow <= nbrOfRow; iRow++) {
     for(let iCol = 1; iCol <= nbrOfCol; iCol++) {
         let col = document.createElement('div');
         col.classList.add('col');
-
+        col.onmouseover = hovered;
         row.appendChild(col);
     }
 
     container.appendChild(row);
 }
 
-
+function hovered() {
+    this.classList.add('hovered');
+}
