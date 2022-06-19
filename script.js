6a80445f8246a4e1452285956784ca72eb6dca58
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-19 18:51:14 +0200

    Implements the resize feature.
    Adds a new button to recover the new value that
    will be used as the height and width of the new grid.
    A new function has been created to remove the existing grid.
    The creation of the grid has been moved into a function
    to ease the creation of the new grid during the resizing process.

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-const nbrOfCol = 16;
-const nbrOfRow = 16;
+let nbrOfCol = 9;
+let nbrOfRow = 9;
 const container = document.getElementById('container');
+const resizeButton = document.getElementById('resizeButton');
 
-for(let iRow = 1; iRow <= nbrOfRow; iRow++) {
-    let row = document.createElement('div')
-    row.classList.add('row');
+resizeButton.addEventListener('click', resize)
+drawGrid();
 
-    for(let iCol = 1; iCol <= nbrOfCol; iCol++) {
-        let col = document.createElement('div');
-        col.classList.add('col');
-        col.onmouseover = hovered;
-        row.appendChild(col);
+function drawGrid() {
+    for (let iRow = 1; iRow <= nbrOfRow; iRow++) {
+        let row = document.createElement('div')
+        row.classList.add('row');
+
+        for (let iCol = 1; iCol <= nbrOfCol; iCol++) {
+            let col = document.createElement('div');
+            col.classList.add('col');
+            col.onmouseover = hovered;
+            row.appendChild(col);
+        }
+
+        container.appendChild(row);
     }
+}
 
-    container.appendChild(row);
+function resize() {
+    let input = prompt('Enter the new size (a number between 9 and 250)');
+    nbrOfCol = input;
+    nbrOfRow = input;
+    clearGrid();
+    drawGrid();
+}
+
+function clearGrid() {
+    let nbrOfChild = container.childElementCount;
+    for (let i = 1; i <= nbrOfChild; i++) {
+        let child = container.firstChild;
+        container.removeChild(child);
+    }
 }
 
 function hovered() {
